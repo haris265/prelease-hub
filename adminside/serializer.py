6d363c5a95e4a3c97397d1195f6d47fc3e2ce184
Haris Ahmed <harisahmedshaikh12@gmail.com>
2026-05-04 01:35:04 +0500

    feat: chnage in buyer property serilizer

--- adminside/serializer.py
+++ adminside/serializer.py
@@ -12,18 +12,68 @@ from user.models import (
 )
 
 
-class BuyerPropertyInquirySerializer(ModelSerializer):
-    buyer_name = serializers.CharField(source='buyer.user_name', read_only=True)
-    property_name = serializers.CharField(source='property.property_name', read_only=True)
+# class BuyerPropertyInquirySerializer(ModelSerializer):
+#     buyer_name = serializers.CharField(source='buyer.user_name', read_only=True)
+#     property_name = serializers.CharField(source='property.property_name', read_only=True)
+#     status = serializers.CharField(source='get_status_display', read_only=True)
+
+#     class Meta:
+#         model = PropertyInquiryModel
+#         fields = [
+#             'id', 'property', 'property_name', 'buyer', 'buyer_name', 
+#             'status', 'meet_link', 'created_at', 'updated_at'
+#         ]
+#         read_only_fields = ['buyer', 'status', 'meet_link']
+
+
+class BuyerPropertyInquirySerializer(serializers.ModelSerializer):
     status = serializers.CharField(source='get_status_display', read_only=True)
+    property_details = serializers.SerializerMethodField(read_only=True)
+    buyer_details = serializers.SerializerMethodField(read_only=True)
+    seller_details = serializers.SerializerMethodField(read_only=True)
 
     class Meta:
         model = PropertyInquiryModel
         fields = [
-            'id', 'property', 'property_name', 'buyer', 'buyer_name', 
-            'status', 'meet_link', 'created_at', 'updated_at'
+            'id', 
+            'property',          # ID for POST request
+            'buyer',             # ID for internal linking
+            'status',            # Status integer
+            # 'status_name',       # Status String
+            'meet_link', 
+            'created_at', 
+            'updated_at',
+            
+            # Nested fields for GET response
+            'property_details',
+            'buyer_details',
+            'seller_details'
         ]
+        
         read_only_fields = ['buyer', 'status', 'meet_link']
+
+  
+    
+    def get_property_details(self, obj):
+        from user.serializer import PropertyListingSerializer 
+        
+        if obj.property:
+            return PropertyListingSerializer(obj.property).data
+        return None
+
+    def get_buyer_details(self, obj):
+        from adminside.serializer import AdminUserSerializer
+        
+        if obj.buyer:
+            return AdminUserSerializer(obj.buyer).data
+        return None
+
+    def get_seller_details(self, obj):
+        from adminside.serializer import AdminUserSerializer
+        
+        if obj.property and obj.property.user:
+            return AdminUserSerializer(obj.property.user).data
+        return None
 
 
 class AdminUserSerializer(ModelSerializer):
